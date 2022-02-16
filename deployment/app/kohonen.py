f2fb0f22a42db0fb4bf31199ab5dd4ed81bced92
Ed Longbottom <eddlong94@gmail.com>
2022-02-16 16:34:45 +1100

    Amended node coordinate array

--- deployment/app/kohonen.py
+++ deployment/app/kohonen.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-class Kohonen():
+class kohonen():
     
     def __init__(self, sizeX, sizeY):
         self.sizeX, self.sizeY = sizeX, sizeY
         self.num_nodes = sizeX*sizeY
-        self.node_coords = [[x,y] for y in range(0, self.sizeY) for x in range(0,self.sizeX)]
+        self.node_coords = np.array([[x,y] for y in range(0, self.sizeY) for x in range(0,self.sizeX)])
         
     def train(self, input_data, nmax_iter):
         ''' Train the SOM with the input training data'''
@@ -31,15 +31,14 @@ class Kohonen():
         
     def get_nbr_nodes(self, bmu_idx, nbr_rad):
         '''Return list of indexes of nodes that fall within neighborhood radius of BMU'''
-        node_coords_arr = np.array(self.node_coords)
-        euc_dis = np.sqrt(np.sum((node_coords_arr - node_coords_arr[bmu_idx])**2, axis=1))
+        euc_dis = np.sqrt(np.sum((self.node_coords - self.node_coords[bmu_idx])**2, axis=1))
         return list(np.where(euc_dis <= nbr_rad)[0])
     
     def update_weights_nbrs(self, t, bmu_idx, V, nbr_nodes, node_weights, time_constant, nbr_rad):
         ''' Returns updated weights for all nodes based on proximity to BMU'''
         learning_rate = 0.1*np.exp(-t/time_constant)
         nbr_node_weights = node_weights[nbr_nodes]
-        nbr_node_coords, bmu_coords  = np.array(self.node_coords)[nbr_nodes], np.array(self.node_coords[bmu_idx])
+        nbr_node_coords, bmu_coords  = self.node_coords[nbr_nodes], self.node_coords[bmu_idx]
 
         euc_dis = np.sqrt(np.sum((bmu_coords - nbr_node_coords)**2,axis=1))
         influences = np.exp(-euc_dis**2 / (2*nbr_rad**2)) 
