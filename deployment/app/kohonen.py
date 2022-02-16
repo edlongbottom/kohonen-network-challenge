c5e4fdfc80c59160d732db8355b54c471954639f
Ed Longbottom <eddlong94@gmail.com>
2022-02-16 14:13:51 +1100

    Initial commit

---
+++ deployment/app/kohonen.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class Kohonen():
+    
+    def __init__(self, sizeX, sizeY):
+        self.sizeX, self.sizeY = sizeX, sizeY
+        self.num_nodes = sizeX*sizeY
+        self.node_coords = [[x,y] for y in range(0, self.sizeY) for x in range(0,self.sizeX)]
+        
+    def train(self, input_data, nmax_iter):
+        ''' Train the SOM with the input training data'''
+        node_weights = np.random.random((self.num_nodes, input_data.shape[1]))
+
+        init_rad = max(self.sizeX, self.sizeY) / 2
+        time_constant = nmax_iter / np.log(init_rad)
+        
+        for idx, V in enumerate(input_data):
+            bmu_idx = self.BMU_calculator(V, node_weights)
+            for t in range(0, nmax_iter):
+                nbr_rad = init_rad*np.exp(-t / time_constant)
+                nbr_nodes = self.get_nbr_nodes(bmu_idx, nbr_rad)
+                node_weights = self.update_weights_nbrs(t, bmu_idx, V, nbr_nodes, node_weights, time_constant, nbr_rad)
+
+        return node_weights
+        
+    
+    def BMU_calculator(self, V, node_weights):
+        '''Returns index of node in node weights that is the BMU'''
+        euc_distances = np.sqrt(np.sum((V - node_weights)**2,axis=1))
+        return np.where(euc_distances == np.min(euc_distances))[0][0]
+        
+    def get_nbr_nodes(self, bmu_idx, nbr_rad):
+        '''Return list of indexes of nodes that fall within neighborhood radius of BMU'''
+        node_coords_arr = np.array(self.node_coords)
+        euc_dis = np.sqrt(np.sum((node_coords_arr - node_coords_arr[bmu_idx])**2, axis=1))
+        return list(np.where(euc_dis <= nbr_rad)[0])
+    
+    def update_weights_nbrs(self, t, bmu_idx, V, nbr_nodes, node_weights, time_constant, nbr_rad):
+        ''' Returns updated weights for all nodes based on proximity to BMU'''
+        learning_rate = 0.1*np.exp(-t/time_constant)
+        nbr_node_weights = node_weights[nbr_nodes]
+        nbr_node_coords, bmu_coords  = np.array(self.node_coords)[nbr_nodes], np.array(self.node_coords[bmu_idx])
+
+        euc_dis = np.sqrt(np.sum((bmu_coords - nbr_node_coords)**2,axis=1))
+        influences = np.exp(-euc_dis**2 / (2*nbr_rad**2)) 
+        node_weights[nbr_nodes] = nbr_node_weights + learning_rate*(influences*(V - nbr_node_weights).T).T 
+        return node_weights
+    
+    def __str__(self):
+        return f'Kohonen network of shape ({self.sizeX},{self.sizeY})'
---
+++ deployment/test_kohonen.py
@@ -0,0 +1,17 @@
+import unittest
+from kohonen import Kohonen
+
+class TestKohonen(unittest.TestCase):
+
+    def test_train(self):
+        self.assertEqual(Kohonen.train(),)
+        self.assertIsInstance(Kohonen.train(),)
+
+    def test_BMU_calculator(self):
+        pass
+
+    def test_update_weights_nbrs(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
